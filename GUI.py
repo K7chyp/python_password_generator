c5c81f6bf472a86ff265950f11d77d7141b37662
K7chyp <nekitos199@gmail.com>
2021-02-25 18:56:09 +0300

    some code view changes

--- GUI.py
+++ GUI.py
@@ -7,8 +7,6 @@ sys.path.append("Path to module")
 import Generator 
 generator = Generator.Generator(16)
 
-
-
 def generate():
 	pass_ = generator.make_it_more_randomly()
 	messagebox.showinfo("Password", f"Your new password is  {pass_}")
@@ -16,14 +14,13 @@ def generate():
 root = tk.Tk()
 
 
-label_1 = tk.Label(root, 
-				text='Password generator and saver').grid(row=0, 
-														column=0)
-label_2 = tk.Label(root, 
-				text='Try to generate your new password').grid(row=1, 
-															column=0)
+label_1 = tk.Label(root,text='Password generator and saver').grid(row=0, 
+								column=0)
+
+label_2 = tk.Label(root,text='Try to generate your new password').grid(row=1, 
+									column=0)
 
 but = tk.Button(root, text="Tap to generate", command=generate).grid(row=2,
-																	column=0)
+								    column=0)
 root.mainloop()
 
